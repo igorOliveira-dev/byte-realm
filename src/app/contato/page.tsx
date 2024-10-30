49d817cf7f1b8a569fe5d8949fd5d79dd0ae8005
Igor <igor.oliveira.contact2210@gmail.com>
2024-10-30 19:43:12 -0300

    correção do tamanho da pagina de contato 2

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function Contato() {
   return (
-    <main className="flex flex-col items-center justify-center only-">
+    <main className="flex flex-col items-center justify-center only-screen">
       <div className="blured-background rounded-lg shadow-md p-8 max-w-lg w-full my-44">
         <h1 className="text-3xl font-bold mb-4 text-center">Fale conosco!</h1>
         <p className="text-lg mb-4 text-center">
