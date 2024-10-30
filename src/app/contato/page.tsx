cb88415f112170e276526095b38d429cbcd49cc8
Igor <igor.oliveira.contact2210@gmail.com>
2024-10-30 19:46:02 -0300

    Corração 3

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -9,7 +9,7 @@ export const metadata = {
 export default function Contato() {
   return (
     <main className="flex flex-col items-center justify-center only-screen">
-      <div className="blured-background rounded-lg shadow-md p-8 max-w-lg w-full my-44">
+      <div className="blured-background rounded-lg shadow-md p-8 max-w-lg mx-6">
         <h1 className="text-3xl font-bold mb-4 text-center">Fale conosco!</h1>
         <p className="text-lg mb-4 text-center">
           Para entrar em contato com a equipe Byte Realm, basta enviar uma mensagem ao email abaixo:
