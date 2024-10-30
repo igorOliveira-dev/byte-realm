d0744c4fcdba1aeecd0a87d35e67dc62b26e94aa
Igor <igor.oliveira.contact2210@gmail.com>
2024-10-30 11:26:53 -0300

    continuando a correção do bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,24 +4,21 @@ import { SanityContextProvider } from "./contexts/sanityContext";
 import Footer from "./components/Footer";
 import Head from "next/head";
 
-const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
-const GOOGLE_SITE_VERIFICATION = process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION;
-
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-br">
       <Head>
-        {/* Google Search verification */}
-        <meta name="google-site-verification" content={GOOGLE_SITE_VERIFICATION} />
+        {/* Google Search Console verification */}
+        <meta name="google-site-verification" content="epEjaq2OfjMrzM1lk7w6rN8etvZPDFC7M6oHsXbYwXQ" />
         {/* Google tag (gtag.js) */}
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9MTGLZV5VH"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${GOOGLE_ANALYTICS_ID}');
+              gtag('config', 'G-9MTGLZV5VH');
             `,
           }}
         />
