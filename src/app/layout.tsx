7295cd27b41121f1169467fe7e8551fa6fcd7c06
Igor <igor.oliveira.contact2210@gmail.com>
2024-10-30 11:33:50 -0300

    mudando a tag Head do next pro head convencional

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,11 @@ import "./globals.css";
 import Header from "./components/Header";
 import { SanityContextProvider } from "./contexts/sanityContext";
 import Footer from "./components/Footer";
-import Head from "next/head";
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-br">
-      <Head>
+      <head>
         {/* Google Search Console verification */}
         <meta name="google-site-verification" content="epEjaq2OfjMrzM1lk7w6rN8etvZPDFC7M6oHsXbYwXQ" />
         {/* Google tag (gtag.js) */}
@@ -22,7 +21,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             `,
           }}
         />
-      </Head>
+      </head>
       <SanityContextProvider>
         <body className="mt-24">
           <Header />
