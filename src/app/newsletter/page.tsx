b4d60ce55a3bd64de660d6ca217b15d99b04a919
Igor <igor.oliveira.contact2210@gmail.com>
2024-12-07 16:55:06 -0300

    front-end da newsletter

--- src/app/newsletter/page.tsx
+++ src/app/newsletter/page.tsx
@@ -12,12 +12,27 @@ export const metadata = {
 
 export default function page() {
   return (
-    <main className="only-screen">
-      <div className={styles.titleBox}>
-        <h2 className={styles.title}>BYTE REALM</h2>
-        <h1 className={styles.titleNewsletter}>Newsletter</h1>
+    <main className={`only-screen ${styles.mainBox}`}>
+      <div className={styles.centerBox}>
+        <div className={styles.titleBox}>
+          <h2 className={styles.title}>BYTE REALM</h2>
+          <h1 className={styles.titleNewsletter}>Newsletter</h1>
+        </div>
+        <p className="center">EM BREVE</p>
+        {/* <p className={styles.mainText}>
+          Nos diga o seu email para receber todas as terças e sextas-feiras as atuais tendências da tecnologia!
+        </p>
+        <form className={styles.form}>
+          <input
+            type="text"
+            placeholder="exemplo@gmail.com"
+            className={`rounded border border-3 custom-border-color blured-background ${styles.emailInput}`}
+          />
+          <button type="submit" className={`rounded ${styles.submitBtn}`}>
+            Inscreva-se
+          </button>
+        </form> */}
       </div>
-      <p className={styles.emBreve}>Em breve</p>
     </main>
   );
 }
