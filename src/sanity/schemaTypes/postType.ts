7a0ca55cf792b4e44b48acab9d76536b3e063507
Igor <igor.oliveira.contact2210@gmail.com>
2024-12-20 15:28:05 -0300

    aaaaaaaa

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -69,6 +69,17 @@ export const postType = defineType({
             ],
           },
         }),
+        defineArrayMember({
+          type: "image",
+          options: { hotspot: true },
+          fields: [
+            {
+              name: "alt",
+              type: "string",
+              title: "Alternative text",
+            },
+          ],
+        }),
       ],
     }),
   ],
