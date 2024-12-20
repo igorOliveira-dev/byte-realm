d99f313c3fb8ac8eaf8ccfa1962cfd89e4e6c042
Igor <igor.oliveira.contact2210@gmail.com>
2024-12-20 15:11:50 -0300

    corrigindo de verdade agora

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -1,22 +1,51 @@
 import { DocumentTextIcon } from "@sanity/icons";
 import { defineArrayMember, defineField, defineType } from "sanity";
+
 export const postType = defineType({
   name: "post",
   title: "Post",
   type: "document",
   icon: DocumentTextIcon,
   fields: [
-    defineField({ name: "title", type: "string" }),
-    defineField({ name: "slug", type: "slug", options: { source: "title" } }),
-    defineField({ name: "author", type: "reference", to: { type: "author" } }),
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+    defineField({
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
+    }),
     defineField({
       name: "mainImage",
       type: "image",
-      options: { hotspot: true },
-      fields: [{ name: "alt", type: "string", title: "Alternative text" }],
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternative text",
+        },
+      ],
     }),
-    defineField({ name: "categories", type: "array", of: [defineArrayMember({ type: "reference", to: { type: "category" } })] }),
-    defineField({ name: "publishedAt", type: "datetime" }),
+    defineField({
+      name: "categories",
+      type: "array",
+      of: [defineArrayMember({ type: "reference", to: { type: "category" } })],
+    }),
+    defineField({
+      name: "publishedAt",
+      type: "datetime",
+    }),
     defineField({
       name: "body",
       type: "array",
@@ -25,19 +54,30 @@ export const postType = defineType({
           type: "block",
           marks: {
             decorators: [],
-            annotations: [{ name: "link", type: "object", fields: [{ name: "href", type: "url", title: "URL" }] }],
+            annotations: [
+              {
+                name: "link",
+                type: "object",
+                fields: [
+                  {
+                    name: "href",
+                    type: "url",
+                    title: "URL",
+                  },
+                ],
+              },
+            ],
           },
-        }),
-        defineArrayMember({
-          type: "image",
-          options: { hotspot: true },
-          fields: [{ name: "alt", type: "string", title: "Alternative text" }],
         }),
       ],
     }),
   ],
   preview: {
-    select: { title: "title", author: "author.name", media: "mainImage" },
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
+    },
     prepare(selection) {
       const { author } = selection;
       return { ...selection, subtitle: author && `by ${author}` };
